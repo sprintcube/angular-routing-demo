cf5fd760b16b7e2ffb3fea2c5a3222b98bf67080
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-13 19:41:41 +0530

    Add album details component

---
+++ src/app/albums/album-details/album-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-album-details',
+  templateUrl: './album-details.component.html',
+  styleUrls: ['./album-details.component.css']
+})
+export class AlbumDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
